ba139bffc83d2fb78c4e28b5c7cc03016ff917a0
Antoine Després <58844676+antoinedespres@users.noreply.github.com>
2021-05-28 10:04:15 +0200

    :lipstick: New style files
    Fonts and colors
    
    Co-Authored-By: Nakoshima <64407502+Nakoshima@users.noreply.github.com>
    Co-Authored-By: HocusPocusCitrus <62442663+HocusPocusCitrus@users.noreply.github.com>

--- wrt-front/src/App.vue
+++ wrt-front/src/App.vue
@@ -9,8 +9,10 @@
 </template>
 
 <style lang="scss">
+@import "./style";
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
+  font-family: "Josefin Sans";
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
