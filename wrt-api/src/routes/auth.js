436c3768ada92a8bf1ecf674d83078b2d7bda0b5
Saibe1111 <sebastien.cuvellier@hotmail.com>
2021-06-09 17:05:01 +0200

    ✨ Add middleware auth

--- wrt-api/src/routes/auth.js
+++ wrt-api/src/routes/auth.js
@@ -1,16 +1,21 @@
 const router = require("express").Router();
 const auth = require("../controllers/auth.js");
-const passport = require('passport');
+const passport = require("passport");
 const config = require("../config.json");
+const { isAuthorized } = require("../middlewares/auth.js");
 
-router.get('/discord', passport.authenticate('discord'));
+router.get("/discord", passport.authenticate("discord"));
 
-router.get('/discord/redirect', passport.authenticate('discord', {
-    failureRedirect: config.front.URL
-}), auth.discordRedirect);
+router.get(
+    "/discord/redirect",
+    passport.authenticate("discord", {
+        failureRedirect: config.front.URL,
+    }),
+    auth.discordRedirect
+);
 
-router.get('/logout', auth.logout);
+router.get("/logout", auth.logout);
 
-router.get('/', auth.getAuth);
+router.get("/", isAuthorized, auth.getAuth);
 
 module.exports = router;
