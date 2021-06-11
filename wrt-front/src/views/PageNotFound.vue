8b97122d617de49a6c82d80df33b20386630f53b
Antoine Després <58844676+antoinedespres@users.noreply.github.com>
2021-06-11 20:50:32 +0200

    :lipstick: New "Page not found" page
    (Error 404)
    
    Co-Authored-By: Nakoshima <64407502+Nakoshima@users.noreply.github.com>
    Co-Authored-By: HocusPocusCitrus <62442663+HocusPocusCitrus@users.noreply.github.com>

---
+++ wrt-front/src/views/PageNotFound.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="d-flex flex-column align-center justify-center">
+    <span :class="$style.bigTitle">404</span>
+    <p :class="$style.subtitle">Page Not Found</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageNotFound",
+};
+</script>
+
+<style lang="scss" module>
+@import "../style";
+
+.bigTitle {
+  color: $color-font-primary;
+  @extend .font-1-extra-large;
+}
+
+.subtitle {
+  color: $color-font-primary;
+  @extend .font-1-large;
+}
+</style>
