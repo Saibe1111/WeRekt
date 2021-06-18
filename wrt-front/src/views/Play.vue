c718460dd9957ef80a7123b7e68b37b3d496f813
Antoine Després <58844676+antoinedespres@users.noreply.github.com>
2021-06-18 20:21:27 +0200

    :lipstick: Update Play page
    - Loading screen
    - Responsive design
    
    Co-Authored-By: HocusPocusCitrus <62442663+HocusPocusCitrus@users.noreply.github.com>
    Co-Authored-By: Nakoshima <64407502+Nakoshima@users.noreply.github.com>

--- wrt-front/src/views/Play.vue
+++ wrt-front/src/views/Play.vue
@@ -1,50 +1,58 @@
 <template>
-  <div class="d-block" :class="$style.sectionHeight">
-    <div class="d-flex justify-center text-center mt-16">
-      <!-- <h1
-        :class="$vuetify.breakpoint.mdAndUp ? $style.title : $style.titleMobile"
-      >
-        What game do you want to
-      </h1>
-      <h1
-        :class="
-          $vuetify.breakpoint.mdAndUp
-            ? $style.coloredTitle
-            : $style.coloredTitleMobile
-        "
-        class="mx-2 px-2"
-      >
-        play
-      </h1>
-      <h1
-        :class="$vuetify.breakpoint.mdAndUp ? $style.title : $style.titleMobile"
-      >
-        ?
-      </h1> -->
-
-      <h1
-        :class="$vuetify.breakpoint.mdAndUp ? $style.title : $style.titleMobile"
-      >
-        What game do you want to
-        <span
+  <div
+    class="d-flex flex-column align-center justify-center"
+    :class="$style.sectionHeight"
+  >
+    <div v-if="!waiting">
+      <div class="d-flex justify-center text-center mt-16">
+        <h1
           :class="
-            $vuetify.breakpoint.mdAndUp
-              ? $style.coloredTitle
-              : $style.coloredTitleMobile
+            $vuetify.breakpoint.mdAndUp ? $style.title : $style.titleMobile
           "
-          class="px-2 py-4 mr-2"
-          >play</span
-        >?
-      </h1>
+        >
+          What game do you want to
+          <span
+            :class="
+              $vuetify.breakpoint.mdAndUp
+                ? $style.coloredTitle
+                : $style.coloredTitleMobile
+            "
+            class="px-2 py-4 mr-2"
+            @click="playBtn()"
+            >play</span
+          >?
+        </h1>
+      </div>
+      <div class="d-flex align-center mt-12 mx-5">
+        <v-text-field
+          class="mr-5"
+          label="Choose a game"
+          v-model="chosenGame"
+          hide-details
+          dark
+          outlined
+        ></v-text-field>
+        <v-btn :class="$style.btn" @click="playBtn()">Play</v-btn>
+      </div>
     </div>
-    <div class="d-flex align-center flex-md-row">
-      <v-text-field
-        label="Choose a game"
-        v-model="chosenGame"
-        dark
-        outlined
-      ></v-text-field>
-      <v-btn :class="$style.btn">Play</v-btn>
+    <div v-else class="d-flex flex-column align-center">
+      <h1
+        :class="$vuetify.breakpoint.mdAndUp ? $style.title : $style.titleMobile"
+      >
+        Finding
+        <span :class="$style.textColor">{{ chosenGame }}</span>
+        players...
+      </h1>
+      <v-progress-circular
+        class="my-10"
+        :size="80"
+        :color="$style.colorSecondary"
+        indeterminate
+      ></v-progress-circular>
+      <span :class="$style.text">players found</span>
+      <v-btn class="mt-7" :class="$style.btn" @click="cancelBtn()"
+        >Cancel</v-btn
+      >
     </div>
   </div>
 </template>
@@ -54,7 +62,20 @@ export default {
   name: "Play",
   components: {},
   data() {
-    return {};
+    return {
+      chosenGame: "",
+      // prendre le nb de gens max + actuel du back
+      waiting: false,
+    };
+  },
+  methods: {
+    playBtn() {
+      // vérifier que le jeu chosenGame existe
+      this.waiting = true;
+    },
+    cancelBtn() {
+      this.waiting = false;
+    },
   },
 };
 </script>
@@ -80,25 +101,8 @@ export default {
   @extend .titleMobile;
 }
 
-.lastSectionHeight {
-  min-height: 80vh !important;
-  display: -webkit-box;
-  display: -webkit-flex;
-  display: -ms-flexbox;
-  display: flex;
-  -webkit-box-align: center;
-  -webkit-align-items: center;
-  -ms-flex-align: center;
-  align-items: center;
-}
-
-.lastSectionHeightMobile {
-  @extend .lastSectionHeight;
-  min-height: 40vh !important;
-}
-
 .sectionHeight {
-  min-height: 70vh !important;
+  min-height: 88vh !important;
   display: -webkit-box;
   display: -webkit-flex;
   display: -ms-flexbox;
@@ -107,18 +111,6 @@ export default {
   -webkit-align-items: center;
   -ms-flex-align: center;
   align-items: center;
-}
-
-.mainTitle {
-  color: $color-font-primary;
-  text-transform: uppercase;
-  @extend .font-1-large-bold;
-}
-
-.mainTitleMobile {
-  color: $color-font-primary;
-  text-transform: uppercase;
-  @extend .font-1-medium-bold;
 }
 
 .title {
@@ -129,5 +121,14 @@ export default {
 .titleMobile {
   color: $color-font-primary;
   @extend .font-1-medium-bold;
+}
+
+.text {
+  color: $color-font-primary;
+  @extend .font-1-medium-small;
+}
+
+.textColor {
+  color: $color-secondary;
 }
 </style>
