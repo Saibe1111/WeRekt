29b266dcb95dfab861519f4dde908f4c9832937f
Saibe1111 <sebastien.cuvellier@hotmail.com>
2021-06-18 15:55:42 +0200

    ✨ User update now accepts images

--- wrt-api/src/routes/user.js
+++ wrt-api/src/routes/user.js
@@ -1,9 +1,27 @@
 const router = require("express").Router();
 const { getUser, updateUser, deleteUser } = require("../controllers/user.js");
-const { isAuthorized } = require("../middlewares/auth.js");
 
-router.get("/", isAuthorized, getUser);
-router.put("/", isAuthorized, updateUser);
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: function (request, file, callback) {
+        callback(null, `${__dirname}/../public/upload/images/banner`);
+    },
+    filename: function (request, file, callback) {
+        let ID = request.query.id || request.user.id;
+        callback(null, `${ID}.png`);
+    },
+});
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fieldSize: 1024 * 1024 * 20,
+    },
+});
+
+router.get("/", getUser);
+router.put("/", upload.single('image'), updateUser);
 router.delete("/", deleteUser);
 
 
