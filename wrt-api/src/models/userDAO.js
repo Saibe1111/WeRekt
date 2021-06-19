2cf30aa5684046b736cbd3d6a055e4b4e12e0c22
OnlyNyZe <61805421+OnlyNyZe@users.noreply.github.com>
2021-06-19 20:40:10 +0200

    🚧 Update userDAO (updateUser and getUser)

--- wrt-api/src/models/userDAO.js
+++ wrt-api/src/models/userDAO.js
@@ -4,9 +4,8 @@ async function createUser(discord_ID, username, Profile_Url) {
     const connection = await database.getConnection();
     let Usname = username.replace(/[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2580-\u27BF]|\uD83E[\uDD10-\uDDFF]/g, "");
 
-
-
-        let sql = "INSERT INTO Users (ID, Username, Profile_Url) values (?,?,?);";
+        let sql = "INSERT INTO Users (ID, Username, Profile_Url,Languages,Platforms,Social_Networks)"+
+        " values (?,?,?,JSON_OBJECT('Languages',JSON_ARRAY()),JSON_OBJECT('Platforms',JSON_ARRAY()),JSON_OBJECT('Social_Networks',JSON_ARRAY()));";
 
         connection.query(sql, [discord_ID, Usname, Profile_Url], (error) => {
             if (error) {
@@ -16,15 +15,14 @@ async function createUser(discord_ID, username, Profile_Url) {
                 console.log("Row inserted");
 
             }
-        }
-
-        )
+        });
         connection.end();
 }
 
 async function getUser(id) {
     const connection = await database.getConnection();
     return new Promise((resolve, reject) => {
+
         let sql = "SELECT * FROM Users where ID=?;";
         connection.query(sql, [id], (error, results) => {
             if (error)
@@ -32,13 +30,19 @@ async function getUser(id) {
             if(results === undefined){
                 resolve(null);
             }else if(results.length > 0){
+
                 resolve({ 
                     id: results[0].ID,
                     username: results[0].Username, 
                     profile_url: results[0].Profile_Url, 
                     description: results[0].Description, 
                     country: results[0].Country, 
-                    birthdate: results[0].Birthdate 
+                    birthdate: results[0].Birthdate,
+                    banner:results[0].Banner,
+                    languages:results[0].Languages,
+                    platforms:results[0].Platforms,
+                    social_networks:results[0].Social_Networks
+
                 });
             }else{
                 resolve(null);
@@ -53,47 +57,75 @@ async function getUser(id) {
 
 }
 
-async function updateUser(discord_ID, Username=undefined, Profile_Url=undefined,Description=undefined, Country=undefined, Birthdate=undefined) {
+async function updateUser(discord_ID, Username=undefined, Profile_Url=undefined,Description=undefined, 
+    Country=undefined, Birthdate=undefined, Banner=undefined, Languages=undefined,Platforms=undefined,Social_Networks=undefined) {
     const connection = await database.getConnection();
 
     let param = [];
-    let usname = "";
-    let purl = "";
-    let desc = "";
-    let cty = "";
-    let bdate = "";
+    let attributes = "";
+
 
     if(Username != undefined){
         let Usname = Username.replace(/[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2580-\u27BF]|\uD83E[\uDD10-\uDDFF]/g, "");
-        usname = "Username=?, ";
+        attributes = attributes + "Username=?, ";
         param.push(Usname);
     }
     if(Profile_Url != undefined){
-        purl = "Profile_Url=?, ";
+        attributes = attributes + "Profile_Url=?, ";
         param.push(Profile_Url);
     }
     if(Description != undefined){
-        desc = "Description=?, ";
+        attributes = attributes + "Description=?, ";
         param.push(Description);
     }
     if(Country != undefined){
-        cty = "Country=?, ";
+        attributes = attributes + "Country=?, ";
         param.push(Country);
     }
     if(Birthdate != undefined){
-        bdate = "Birthdate=str_to_date(?,'%Y-%m-%d'), ";
+        attributes = attributes + "Birthdate=str_to_date(?,'%Y-%m-%d'), ";
         param.push(Birthdate);
+    }
+    if(Banner != undefined){
+        attributes = attributes + "Banner=?, "
+        param.push(Banner);
+    }
+    if(Languages !=undefined){
+      
+        attributes = attributes + "Languages = JSON_SET(`Languages`,";
+        for (var i = 0; i < Languages.length; i++) {
+            attributes = attributes + `"$.Languages[${i}]",?, `;
+            param.push(Languages[i]);
+          }
+          attributes = attributes.replace(/,\s*$/, "") + "), "
+
+    }
+
+    if(Platforms != undefined){
+        attributes = attributes + "Platforms = JSON_SET(`Platforms`,";
+        for (var i = 0; i < Platforms.length; i++) {
+            attributes = attributes + `"$.Platforms[${i}]",JSON_OBJECT("name",?,"username",?), `;
+            param.push(Platforms[i].name);
+            param.push(Platforms[i].username);
+          }
+          attributes = attributes.replace(/,\s*$/, "") + "), "
+    }
+
+    if(Social_Networks !=undefined){
+        console.log(Social_Networks);
+        attributes = attributes + "Social_Networks = JSON_SET(`Social_Networks`, ";
+        for (var i = 0; i < Social_Networks.length; i++) {
+            attributes = attributes + `"$.Social_Networks[${i}]",JSON_OBJECT("name",?,"username",?), `;
+            param.push(Social_Networks[i].name);
+            param.push(Social_Networks[i].username);
+          }
+          attributes = attributes.replace(/,\s*$/, "") + "), "
     }
 
     param.push(discord_ID);
 
-    let attributes = usname+purl+desc+cty+bdate;
-
-
-
-
         let sql = `UPDATE Users SET ${attributes.replace(/,\s*$/, "")} WHERE ID = ?;`;
-
+        console.log(sql);
         connection.query(sql, param, (error) => {
             if (error) {
                 console.error(error.message);
