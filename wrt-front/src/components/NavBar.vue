8498b767617b6d26afbe793f6e104a332abb1437
Antoine Després <58844676+antoinedespres@users.noreply.github.com>
2021-06-22 19:12:01 +0200

    :lipstick: Update navbar style
    Co-Authored-By: HocusPocusCitrus <62442663+HocusPocusCitrus@users.noreply.github.com>
    Co-Authored-By: Nakoshima <64407502+Nakoshima@users.noreply.github.com>

--- wrt-front/src/components/NavBar.vue
+++ wrt-front/src/components/NavBar.vue
@@ -113,6 +113,10 @@ export default {
   background-clip: text;
   -webkit-text-fill-color: transparent;
   text-decoration: none;
+  -webkit-user-select: none; /* Safari */
+  -moz-user-select: none; /* Firefox */
+  -ms-user-select: none; /* IE10+/Edge */
+  user-select: none; /* Standard */
   @extend .font-1-medium;
 }
 
