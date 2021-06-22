a4f49a0c322ef96f84e780f16a16d921d6352ce6
Saibe1111 <sebastien.cuvellier@hotmail.com>
2021-06-22 11:18:58 +0200

    🔥 Suppression of the unnecessary

--- wrt-api/src/listeners/userConnected.js
+++ wrt-api/src/listeners/userConnected.js
@@ -7,39 +7,5 @@ module.exports = function (socket) {
             getRoom(socket, user);
         }
         
-        //Messages
-
-        
-
-        let messages = [
-            {
-                content: "Coucou",
-                timestamp: new Date(),
-                sender: "Antoine",
-                senderId: "345823189449965579",
-            },
-            {
-                content: "Coucou",
-                timestamp: new Date(),
-                sender: "Monica",
-                senderId: "283639048483110922",
-            },
-            {
-                content:
-                    "Hey !",
-                timestamp: new Date(),
-                sender: "Seb",
-                senderId: "181782320494280704",
-            },
-        ];
-        //socket.emit("room_Info", messages, members);
-        let message = {
-            content:
-                "cv ?",
-            timestamp: new Date(),
-            sender: "Vivi",
-            senderId: "578957887108546571",
-        };
-        //socket.emit("room_Info", message);
     });
 };
