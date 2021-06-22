32c53ee945c135a49a055ad71924c39124e87ffa
Saibe1111 <sebastien.cuvellier@hotmail.com>
2021-06-22 12:07:55 +0200

    🐛 Fix timestamp

--- wrt-api/src/listeners/message.js
+++ wrt-api/src/listeners/message.js
@@ -5,7 +5,7 @@ const moment = require('moment');
 
 module.exports = async function (socket) {
     socket.on("message", async function  (message) {
-        messageDAO.createMessage(message.content, moment(message.timestamp, 'DD/MM/YYYY HH:mm:ss').format('YYYY-MM-DD HH:MM:SS'), message.senderId, message.room);
+        messageDAO.createMessage(message.content, moment(message.timestamp, 'DD/MM/YYYY HH:mm:ss').format('YYYY-MM-DD HH:mm:ss'), message.senderId, message.room);
         let user = await userDAO.getUser(message.senderId);
         message.sender = user.username;
         emitInRoom(socket, "new_message", message.room, message);
